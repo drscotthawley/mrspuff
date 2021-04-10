abc1eab71ffde05ed7ccf1e3a1e4249e4e910a58
drscotthawley <scott.hawley@belmont.edu>
2021-04-10 01:17:58 -0500

    removing tutorial code, adding viz & utils

--- mrspuff/_nbdev.py
+++ mrspuff/_nbdev.py
@@ -2,25 +2,26 @@
 
 __all__ = ["index", "modules", "custom_doc_links", "git_url"]
 
-index = {"say_hello": "00_core.ipynb",
-         "Card": "01_card.ipynb",
-         "Deck": "02_deck.ipynb",
-         "fetch_image_urls": "03_scrape.ipynb",
-         "download_and_save": "03_scrape.ipynb",
-         "search_and_download": "03_scrape.ipynb",
-         "Category": "03_scrape.ipynb",
-         "img_scrape": "03_scrape.ipynb",
-         "browse_images": "03_scrape.ipynb",
-         "get_pca_xy": "03_scrape.ipynb",
-         "plot_hover": "03_scrape.ipynb",
-         "plot_triangle": "03_scrape.ipynb",
-         "nimport": "04_nimport.ipynb"}
+index = {"nimport": "nimport.ipynb",
+         "fetch_image_urls": "scrape.ipynb",
+         "download_and_save": "scrape.ipynb",
+         "search_and_download": "scrape.ipynb",
+         "Category": "scrape.ipynb",
+         "img_scrape": "scrape.ipynb",
+         "browse_images": "scrape.ipynb",
+         "get_pca_xy": "scrape.ipynb",
+         "plot_hover": "scrape.ipynb",
+         "plot_triangle": "scrape.ipynb",
+         "softmax": "utils.ipynb",
+         "calc_prob": "utils.ipynb",
+         "triangle3d_plotly": "viz.ipynb",
+         "TrianglePlotBokeh": "viz.ipynb",
+         "VizPreds": "viz.ipynb"}
 
-modules = ["core.py",
-           "card.py",
-           "deck.py",
+modules = ["nimport.py",
            "scrape.py",
-           "nimport.py"]
+           "utils.py",
+           "viz.py"]
 
 doc_url = "https://drscotthawley.github.io/mrspuff/"
 
